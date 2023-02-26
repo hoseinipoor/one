b745b78b8e9460dba87f43c646e767ee5a0dbb12
somayyeh <hoseinipoor0071@gmail.com>
2023-02-26 12:35:52 +0330

    'change

--- Ex3.py
+++ Ex3.py
@@ -1,3 +1,18 @@
-c={'a':1,'b':2}
+#Write a Python function to sum all the numbers in a list
+#based on question,input is a list 
+def sum_func(input_list):
+    sum_value = 0
+    for num in input_list:
+        sum_value  += num 
+    return sum_value
 
-print(type(c))
+
+N = int(input('How many numbers should be sum: '))
+numbers = []
+for i in range(N):
+    num = float(input(f'Enter {i+1}th number:'))
+    numbers.append(num)
+
+result = sum_func(numbers)
+print(f'sum value is {result}')
+
