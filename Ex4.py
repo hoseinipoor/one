8bc47ae831966a91ae12f1cc47ae2d779cfbf1f3
somayyeh <hoseinipoor0071@gmail.com>
2023-02-26 12:38:48 +0330

    change Ex4.py

--- Ex4.py
+++ Ex4.py
@@ -1,1 +1,19 @@
-c={'a':1,'b':2}
+#Write a Python function to multiply all the numbers in a list
+#based on question,input is a list 
+def multiply_func(input_list):
+    multiply_value = 1
+    for num in input_list:
+        multiply_value  *= num 
+    return multiply_value
+
+
+N = int(input('How many numbers should be multipled: '))
+numbers = []
+for i in range(N):
+    num = float(input(f'Enter {i+1}th number:'))
+    numbers.append(num)
+
+result = multiply_func(numbers)
+print(f'multiply value is {result}')
+
+
